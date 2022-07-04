87e613172a969eab8d523137148360502065e4a1
Charly Cimino <carlosecimino@gmail.com>
2022-07-04 15:43:16 -0300

    Terminado

---
+++ src/metodosdefault2/Empleado.java
@@ -0,0 +1,14 @@
+package metodosdefault2;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+public interface Empleado {
+
+    public default void realizarTarea() {
+        System.out.println("Realizo tarea porque me pagan por ello");
+    }
+}
---
+++ src/metodosdefault2/Persona.java
@@ -0,0 +1,17 @@
+package metodosdefault2;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/CharlyCimino
+ */
+public class Persona implements Empleado, Estudiante {
+
+    @Override
+    public void realizarTarea() {
+        Estudiante.super.realizarTarea();
+        //Empleado.super.realizarTarea();
+    }
+
+}
